6c8398ce447877ae73f74976edca91bf1bcad33f
Venkat Sai <vainalavenkatsai@gmail.com>
2025-06-16 13:31:31 -0400

    Initial Commit 3

---
+++ venkat-portfolio.io/src/App.jsx
@@ -0,0 +1,17 @@
+import Hero from './Hero';
+import About from './About';
+import Projects from './Projects';
+import Contact from './Contact';
+
+function App() {
+  return (
+    <div className="App">
+      <Hero />
+      <About />
+      <Projects />
+      <Contact />
+    </div>
+  );
+}
+
+export default App;
---
+++ venkat-portfolio.io/src/Hero.jsx
@@ -0,0 +1,11 @@
+function Hero() {
+  return (
+    <div className="hero">
+      <h1>Hi, I'm Venkat Sai</h1>
+      <p>Full Stack Developer | MS in IT @ UNH</p>
+      <a href="#contact" className="btn">Get in Touch</a>
+    </div>
+  );
+}
+
+export default Hero;
